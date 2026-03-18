26069b187740deb1ad693a58b555e1ac570f50a7
Lucas Gabriel <lucasgabrielwww2218@gmail.com>
2026-03-18 00:45:16 -0300

    Updates

--- assets/404-CNx6u6zt.js
+++ assets/404-CNx6u6zt.js
@@ -1,4 +1,4 @@
-import{c as t,j as e}from"./index-2WAivdrN.js";import{A as o}from"./arrow-left-BaRJfKB5.js";/**
+import{c as t,j as e}from"./index-abBzNrGA.js";import{A as o}from"./arrow-left-C9vTwZRn.js";/**
  * @license lucide-react v0.454.0 - ISC
  *
  * This source code is licensed under the ISC license.
