8bca2470faeca1ff5bee2958720104692a7f26e2
Lucas Gabriel <lucasgabrielwww2218@gmail.com>
2026-03-17 22:32:00 -0300

    Updates

--- assets/code-xml-BI0yaVna.js
+++ assets/code-xml-BI0yaVna.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-BKG_1LJI.js";/**
+import{c as e}from"./index-Cu3MajCk.js";/**
  * @license lucide-react v0.454.0 - ISC
  *
  * This source code is licensed under the ISC license.
