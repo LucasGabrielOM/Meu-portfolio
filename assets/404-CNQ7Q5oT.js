fce2ad1f5ea0db7890eb8fb605206124462d4c7c
Lucas Gabriel <lucasgabrielwww2218@gmail.com>
2026-03-18 18:39:38 -0300

    Updates

--- assets/404-CNQ7Q5oT.js
+++ assets/404-CNQ7Q5oT.js
@@ -1,4 +1,4 @@
-import{c as t,j as e}from"./index-BtJDqQUg.js";import{A as o}from"./arrow-left-VrfZjUM1.js";/**
+import{c as t,j as e}from"./index-B18gDDnM.js";import{A as o}from"./arrow-left-BtkDQgXr.js";/**
  * @license lucide-react v0.454.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/ProjectDetail-mi9qhOkg.js
+++ assets/ProjectDetail-mi9qhOkg.js
@@ -1,4 +1,4 @@
-import{c as i,u as v,h as j,r as p,j as e,H as y,i as w,E as N,G as k,k as L,C as n}from"./index-BtJDqQUg.js";import{S as T}from"./sweetalert2.esm.all-CIewtpap.js";import{A as I}from"./arrow-left-VrfZjUM1.js";import{C as g}from"./code-xml-Czrh_HNm.js";/**
+import{c as i,u as v,h as j,r as p,j as e,H as y,i as w,E as N,G as k,k as L,C as n}from"./index-B18gDDnM.js";import{S as T}from"./sweetalert2.esm.all-CIewtpap.js";import{A as I}from"./arrow-left-BtkDQgXr.js";import{C as g}from"./code-xml-1AUBZaV3.js";/**
  * @license lucide-react v0.454.0 - ISC
  *
  * This source code is licensed under the ISC license.
