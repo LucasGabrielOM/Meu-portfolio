da521d061f9088e2e6ed9078bae38a1c857572f2
Lucas Gabriel <lucasgabrielwww2218@gmail.com>
2026-03-17 22:10:18 -0300

    Updates

--- assets/404-kqnHFtdt.js
+++ assets/404-kqnHFtdt.js
@@ -1,4 +1,4 @@
-import{c as t,j as e}from"./index-Cd8nCK5M.js";import{A as o}from"./arrow-left-PO2GTXmB.js";/**
+import{c as t,j as e}from"./index-BKG_1LJI.js";import{A as o}from"./arrow-left-BYSxIBlm.js";/**
  * @license lucide-react v0.454.0 - ISC
  *
  * This source code is licensed under the ISC license.
