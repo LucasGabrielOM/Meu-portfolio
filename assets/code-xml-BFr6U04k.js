31ef86477d8127580be46f6b87687169592f69d9
LucasGabrielOM <LucasGabrielOM@users.noreply.github.com>
2026-07-30 01:17:51 +0000

    deploy: 8c1a4361d92832497ad1ac82467ed79a6ade96f0

--- assets/code-xml-BFr6U04k.js
+++ assets/code-xml-BFr6U04k.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-BOmwH2YM.js";/**
+import{c as e}from"./index-CbnESovx.js";/**
  * @license lucide-react v0.454.0 - ISC
  *
  * This source code is licensed under the ISC license.
